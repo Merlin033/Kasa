f5f84198dfddf96484d1602f7963f3f057ac5f1d
Merlin033 <barnabedebocq@live.fr>
2023-06-25 00:59:43 +0200

    animation pannel + footer

--- src/layout/Footer.jsx
+++ src/layout/Footer.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 import "./Footer.scss";
 function Footer() {
-  return (
+  return (<div className="footer__container">
     <div className="footer">
       <div>
         <img src="logo-black.png" alt="logo" width="100" />
       </div>
       <div>© 2020 Kasa. All rights reserved</div>
-    </div>
+    </div></div>
   );
 }
 
